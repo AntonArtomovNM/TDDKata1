170dda8cdea718db16623a8659f2db904b90d16c
Anton Artomov <86731073+AntonArtomovNM@users.noreply.github.com>
2021-08-05 15:34:04 +0300

    Add Add() method

--- StringCalculator/Kata1.XUnitTests/UnitTest1.cs
+++ StringCalculator/Kata1.XUnitTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using StringCalculators;
 
 namespace Kata1.XUnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData ("", 0)]
+        [InlineData ("0", 0)]
+        [InlineData ("1", 1)]
+        [InlineData ("1,3", 4)]
+        [InlineData ("1,3,5", 9)]
+        public void AddNumsCorrect(string numbers, int expected)
         {
+            StringCalculator calculator = new StringCalculator();
 
+            var actual = calculator.Add(numbers);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculators
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var numbersArray = numbers.Trim().Split(',').Select(x => int.Parse(x));
+            return numbersArray.Aggregate((x, y) => x + y);
+        }
+    }
+}
